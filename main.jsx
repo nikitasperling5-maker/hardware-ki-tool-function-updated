0d9bd120c520723a5180a3320062dca6ffcbc33f
nikitasperling5-maker <nikitasperling5@gmail.com>
2025-08-02 01:14:34 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HardwareAdvisor from "./HardwareAdvisor";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HardwareAdvisor />
+  </React.StrictMode>
+);
